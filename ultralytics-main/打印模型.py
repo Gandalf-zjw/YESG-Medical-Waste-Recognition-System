15ea445124e8c779c43aa0b37f21afd5ade6ff44
Gandalf-zjw <2332733774@qq.com>
2025-11-19 16:53:49 +0800

    Delete ultralytics-main/打印模型.py

--- ultralytics-main/打印模型.py
+++
@@ -1,3 +0,0 @@
-from ultralytics import YOLO
-model = YOLO(r"D:\YESG\Medical Waste Recognition System\ultralytics-main\ultralytics\cfg\models\11\yolo11.yaml")  # 替换为你的YAML路径
-print(model.model)  # 查看模型结构是否包含SwinTransformer
